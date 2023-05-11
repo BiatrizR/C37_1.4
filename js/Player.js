dd66e38c56a1aa01f98c7156ea24e52eda624159
Bia <82560900+BiatrizR@users.noreply.github.com>
2023-05-11 19:00:37 -0300

    Update Player.js

--- js/Player.js
+++ js/Player.js
@@ -1,44 +1,24 @@
 class Player {
   constructor() {
-    //adicionando propriedades do jogador
-    this.name = null;
-    this.index = null;
-    this.positionX = 0;
-    this.positionY = 0;
-  }
+   this.name = null;
+   this.index = null;
+   this.positionX = 0;
+   this.positionY = 0;
+ }
 
-  addPlayer() {
-    var playerIndex = "players/player" + this.index;
+ addPlayer() {
+   var playerIndex = "players/player" + this.index;
 
-    if (this.index === 1) {
-      this.positionX = width / 2 - 100;
-    } else {
-      this.positionX = width / 2 + 100;
-    }
+   if (this.index === 1) {
+     this.positionX = width / 2 - 100;
+   } else {
+     this.positionX = width / 2 + 100;
+   }
 
-    database.ref(playerIndex).set({
-      name: this.name,
-      positionX: this.positionX,
-      positionY: this.positionY
-    });
-
-  }
-
-  getCount() {
-    var playerCountRef = database.ref("playerCount");
-    playerCountRef.on("value", data => {
-      playerCount = data.val();
-    });
-  }
-
-//criar função de update count
-
-  update(){
-
-    var playerIndex = "players/player" + this.index;
-    database.ref(playerIndex).update({
-      positionX: this.positionX,
-      positionY: this.positionY,
-    });
-  }
+   database.ref(playerIndex).set({
+     name: this.name,
+     positionX: this.positionX,
+     positionY: this.positionY,
+   });
+ } 
 }
