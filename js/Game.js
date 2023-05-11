9db9ea44cb74868f5615a89a0daa741846f52b31
Bia <82560900+BiatrizR@users.noreply.github.com>
2023-05-11 19:00:16 -0300

    Update Game.js

--- js/Game.js
+++ js/Game.js
@@ -1,39 +1,12 @@
 class Game {
   constructor() {}
 
-  getState() {
-    var gameStateRef = database.ref("gameState");
-    gameStateRef.on("value", function(data) {
-      gameState = data.val();
-    });
-  }
-
-  update(state){
-    database.ref("/").update({
-      gameState: state
-    });
-  }
-
   start() {
-   // CHAMAR PLAYER 
+    // INCLUA A CLASSE DO JOGO
     
-    //CHAMAR FORM
-
-    car1 = createSprite(width/2 - 50, height - 100);
-    car1.addImage("car1", car1_img);
-    car1.sale = 0.08;
-
-  }
-
-
-  handleElements(){
-    form.hide();
-    form.titleImg.position(40, 40);
-    form.titleImg.class("gameTitleAfterEffects");
-  }
-
-  play(){
-    this.handleElements();
-    drawSprite();
+   
+   
+    //! 5.1 chamar classe
+   
   }
 }
